69f16d11b57826b411084a0c82404b91fb543627
Danilo <daniloturini@gmail.com>
2023-05-08 21:28:46 -0300

    HEY-2: Pint correção

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\User;
-use \Illuminate\Contracts\Validation\Rule as ContractRule;
+use Illuminate\Contracts\Validation\Rule as ContractRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
