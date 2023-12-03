7ed8a5b3e45ac705d10532582f6d1c6221bedcbc
Danilo <daniloturini@gmail.com>
2023-12-03 08:40:34 -0300

    HEY-11: refactoring to create an action method on user model to like a question

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,4 +45,21 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return HasMany<vote>
+     */
+    public function votes(): HasMany
+    {
+        return $this->hasMany(vote::class);
+    }
+
+    public function like(Question $question): void
+    {
+        $this->votes()->create([
+            'question_id' => $question->id,
+            'like'        => 1,
+            'unlike'      => 0,
+        ]);
+    }
 }
